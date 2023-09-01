a99de1f872014ec3ddd615440a4691a1d97d3d5c
Pyaephyo33 <pyaephyolinn778899@gmail.com>
2023-09-01 16:48:52 +0630

    first commit

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Task;
+use Illuminate\Support\Facades\Validator;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $tasks = Task::all();
+        return response()->json(['tasks' => $tasks]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // dd($request->all());
+        $validator = Validator::make($request->all(),[
+            'title' => 'required'
+        ]);
+
+        if($validator->fails()){
+            return response()->json($validator->messages()->add('status','fails'));
+        }
+
+        $data = Task::create($validator->validated());
+        return response()->json(['status' => 'passes', 'data' => $data]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
